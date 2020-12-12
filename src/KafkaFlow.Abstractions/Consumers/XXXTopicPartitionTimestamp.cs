35ff6706b23fa2f56909207eb6e4bf5962bd365a
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-12 13:25:04 +0800

    update

---
+++ src/KafkaFlow.Abstractions/Consumers/XXXTopicPartitionTimestamp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaFlow.Consumers
+{
+    public class XXXTopicPartitionTimestamp
+    {
+        public XXXTopicPartitionTimestamp(XXXTopicPartition tp, DateTimeOffset timestamp)
+        {
+
+        }
+        public XXXTopicPartitionTimestamp(string topic, int partition, DateTimeOffset timestamp)
+        {
+
+        }
+        public string Topic { get; }
+      
+        public int Partition { get; }
+      
+        public DateTimeOffset Timestamp { get; }
+       
+        public XXXTopicPartition TopicPartition { get; }
+    }
+}
