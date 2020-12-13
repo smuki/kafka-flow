5199d0ecb8ab61993bb38407cc55eb596a6b07af
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-13 18:57:34 +0800

    Update IConsumerClient.cs

--- src/KafkaFlow.Abstractions/Consumers/IConsumerClient.cs
+++ src/KafkaFlow.Abstractions/Consumers/IConsumerClient.cs
@@ -8,28 +8,11 @@ using System.Threading.Tasks;
 
 namespace KafkaFlow.Consumers
 {
-    /// <inheritdoc />
     /// <summary>
     /// Message queue consumer client
     /// </summary>
     public interface IConsumerClient : IDisposable
     {
-        //string BrokerAddress { get; }
-
-        /// <summary>
-        /// Subscribe to a set of topics to the message queue
-        /// </summary>
-        /// <param name="topics"></param>
-        //void Subscribe(IEnumerable<string> topics);
-
-        /// <summary>
-        /// Start listening
-        /// </summary>
-        //void Listening(TimeSpan timeout, CancellationToken cancellationToken);
-
-        /// <summary>
-        /// Manual submit message offset when the message consumption is complete
-        /// </summary>
         void Commit(IEnumerable<XXXTopicPartitionOffset> offsets);
         void Pause(IEnumerable<XXXTopicPartition> offsets);
         void Resume(IEnumerable<XXXTopicPartition> offsets);
@@ -44,11 +27,5 @@ namespace KafkaFlow.Consumers
         Task StartAsync();
 
         Task StopAsync();
-        /// <summary>
-        /// Reject message and resumption
-        /// </summary>
-        //void Reject(object sender);
-        void OnPartitionRevoked(IConsumerClient consumer, IReadOnlyCollection<XXXTopicPartitionOffset> topicPartitions);
-        void OnPartitionAssigned(IConsumerClient consumer, IReadOnlyCollection<XXXTopicPartition> partitions);
     }
 }
