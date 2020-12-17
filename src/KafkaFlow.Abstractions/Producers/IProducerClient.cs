cd7072836f0de43a1980e2de00a473fa2c22cd8a
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-17 20:55:08 +0800

    Create IProducerClient.cs

---
+++ src/KafkaFlow.Abstractions/Producers/IProducerClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaFlow.Producers
+{
+    /// <summary>
+    /// Message queue producer client
+    /// </summary>
+    public interface IProducerClient
+    {
+
+    }
+}
