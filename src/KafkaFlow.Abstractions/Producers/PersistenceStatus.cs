8c74d1907a9fa67c56dbdf5f85f375d6a544b3b1
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-17 21:55:06 +0800

    Create PersistenceStatus.cs

---
+++ src/KafkaFlow.Abstractions/Producers/PersistenceStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaFlow.Producers
+{
+    public enum PersistenceStatus
+    {
+        NotPersisted = 0,
+        PossiblyPersisted = 1,
+        Persisted = 2
+    }
+}
