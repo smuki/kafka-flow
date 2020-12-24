229a861088ab54b49386d6836f1bf91de8db4851
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-25 07:45:14 +0800

    Update IConsumerClient.cs

--- src/KafkaFlow.Abstractions/Consumers/IConsumerClient.cs
+++ src/KafkaFlow.Abstractions/Consumers/IConsumerClient.cs
@@ -14,6 +14,7 @@ namespace KafkaFlow.Consumers
     /// </summary>
     public interface IConsumerClient : IDisposable
     {
+        void Initialize(EventConsumer eventConsumer);
         void Commit(IEnumerable<XXXTopicPartitionOffset> offsets);
         void Pause(IEnumerable<XXXTopicPartition> offsets);
         void Resume(IEnumerable<XXXTopicPartition> offsets);
