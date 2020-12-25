4624b4cb6e5ba07d82b10834e2c867f653078b74
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-25 15:38:28 +0800

    Update XXXProduceException.cs

--- src/KafkaFlow.Abstractions/XXXProduceException.cs
+++ src/KafkaFlow.Abstractions/XXXProduceException.cs
@@ -4,9 +4,6 @@ using System.Text;
 
 namespace KafkaFlow
 {
-    //
-    // 摘要:
-    //     Represents an error that occured whilst producing a message.
     public class XXXProduceException : Exception
     {
 
@@ -20,16 +17,13 @@ namespace KafkaFlow
             this.DeliveryResult = deliveryResult;
             this.Error = error;
         }
-      
+
         public XXXProduceException(XXXError error, XXXDeliveryResult deliveryResult, Exception innerException)
         {
             this.DeliveryResult = deliveryResult;
             this.Error = error;
         }
         public XXXError Error { get; set; }
-        //
-        // 摘要:
-        //     The delivery result associated with the produce request.
         public XXXDeliveryResult DeliveryResult { get; set; }
     }
 }
