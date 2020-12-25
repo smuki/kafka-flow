fd96f0b2b23db6c58374464db41588da3930d475
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-25 12:06:55 +0800

    Update BytesSumDistributionStrategy.cs

--- src/KafkaFlow.Abstractions/Consumers/DistributionStrategies/BytesSumDistributionStrategy.cs
+++ src/KafkaFlow.Abstractions/Consumers/DistributionStrategies/BytesSumDistributionStrategy.cs
@@ -18,7 +18,7 @@ namespace KafkaFlow.Consumers.DistributionStrategies
         /// Initializes the distribution strategy
         /// </summary>
         /// <param name="workers"></param>
-        public void Init(IReadOnlyList<IWorker> workers)
+        public void Initialize(IReadOnlyList<IWorker> workers)
         {
             this.workers = workers;
         }
