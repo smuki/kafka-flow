ba0a4d2f08a48c50cb4b9f6d0b63bea7bb62e5e2
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-25 21:20:02 +0800

    Create KafkaFlowOptions.cs

---
+++ src/KafkaFlow.Abstractions/KafkaFlowOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaFlow
+{
+    public class KafkaFlowOptions
+    {
+        public IServiceCollection Services { get; }
+
+        public KafkaFlowOptions(IServiceCollection services)
+        {
+            Services = services;
+        }
+    }
+}
