687c30e92a21d6126ffa0447dd3c30c65d7b12bb
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-25 16:35:32 +0800

    Update MicrosoftDependencyConfigurator.cs

--- src/KafkaFlow.Microsoft.DependencyInjection/MicrosoftDependencyConfigurator.cs
+++ src/KafkaFlow.Microsoft.DependencyInjection/MicrosoftDependencyConfigurator.cs
@@ -18,11 +18,7 @@ namespace KafkaFlow.Microsoft.DependencyInjection
             Type implementationType,
             InstanceLifetime lifetime)
         {
-            this.services.Add(
-                ServiceDescriptor.Describe(
-                    serviceType,
-                    implementationType,
-                    ParseLifetime(lifetime)));
+            this.services.Add(ServiceDescriptor.Describe(serviceType, implementationType, ParseLifetime(lifetime)));
 
             return this;
         }
@@ -31,24 +27,14 @@ namespace KafkaFlow.Microsoft.DependencyInjection
             where TService : class
             where TImplementation : class, TService
         {
-            this.services.Add(
-                ServiceDescriptor.Describe(
-                    typeof(TService),
-                    typeof(TImplementation),
-                    ParseLifetime(lifetime)));
-
+            this.services.Add(ServiceDescriptor.Describe(typeof(TService), typeof(TImplementation), ParseLifetime(lifetime)));
             return this;
         }
 
         public IDependencyConfigurator Add<TService>(InstanceLifetime lifetime)
             where TService : class
         {
-            this.services.Add(
-                ServiceDescriptor.Describe(
-                    typeof(TService),
-                    typeof(TService),
-                    ParseLifetime(lifetime)));
-
+            this.services.Add(ServiceDescriptor.Describe(typeof(TService), typeof(TService), ParseLifetime(lifetime)));
             return this;
         }
 
@@ -64,12 +50,7 @@ namespace KafkaFlow.Microsoft.DependencyInjection
             Func<IDependencyResolver, TImplementation> factory,
             InstanceLifetime lifetime)
         {
-            this.services.Add(
-                ServiceDescriptor.Describe(
-                    serviceType,
-                    provider => factory(new MicrosoftDependencyResolver(provider)),
-                    ParseLifetime(lifetime)));
-
+            this.services.Add(ServiceDescriptor.Describe(serviceType, provider => factory(new MicrosoftDependencyResolver(provider)), ParseLifetime(lifetime)));
             return this;
         }
 
@@ -87,10 +68,7 @@ namespace KafkaFlow.Microsoft.DependencyInjection
                     return ServiceLifetime.Transient;
 
                 default:
-                    throw new ArgumentOutOfRangeException(
-                        nameof(lifetime),
-                        lifetime,
-                        null);
+                    throw new ArgumentOutOfRangeException(nameof(lifetime), lifetime, null);
             }
         }
     }
