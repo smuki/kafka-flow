25cc24d04f1c5e63e16f2de62c1ecb94242a3278
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-25 15:33:13 +0800

    Update ProducerAccessor.cs

--- src/KafkaFlow.Abstractions/Producers/ProducerAccessor.cs
+++ src/KafkaFlow.Abstractions/Producers/ProducerAccessor.cs
@@ -12,14 +12,28 @@ namespace KafkaFlow.Producers
             this.producers = producers.ToDictionary(x => x.ProducerName);
         }
 
-        public IMessageProducer GetProducer(string name) =>
-            this.producers.TryGetValue(name, out var consumer) ? consumer : null;
+        public IMessageProducer GetProducer(string name)
+        {
+            return this.producers.TryGetValue(name, out var consumer) ? consumer : null;
+        }
+        public IMessageProducer GetProducer<TProducer>()
+        {
+            return   this.producers.TryGetValue(typeof(TProducer).FullName, out var consumer) ? consumer : null;
+        }
+        public IEnumerable<IMessageProducer> Producers 
+        {
+            get
+            {
+                return this.producers.Values;
+            }
+        }
 
-        public IMessageProducer GetProducer<TProducer>() =>
-            this.producers.TryGetValue(typeof(TProducer).FullName, out var consumer) ? consumer : null;
-
-        public IEnumerable<IMessageProducer> All => this.producers.Values;
-
-        public IMessageProducer this[string name] => this.GetProducer(name);
+        public IMessageProducer this[string name]
+        {
+            get
+            {
+                return this.GetProducer(name);
+            }
+        }
     }
 }
