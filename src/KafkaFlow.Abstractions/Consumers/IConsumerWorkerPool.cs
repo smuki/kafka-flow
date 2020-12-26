c3c1223de484a0c779b5c96f13d139deac9973fd
yjy <yjy@ekuaibao.com>
2020-12-26 18:17:38 +0800

    Update IConsumerWorkerPool.cs

--- src/KafkaFlow.Abstractions/Consumers/IConsumerWorkerPool.cs
+++ src/KafkaFlow.Abstractions/Consumers/IConsumerWorkerPool.cs
@@ -3,9 +3,12 @@ namespace KafkaFlow.Consumers
     using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
+    using KafkaFlow.Configuration;
 
     public interface IConsumerWorkerPool
     {
+        void Initialize(ConsumerSetting eventConsumer);
+
         Task StartAsync(IConsumerClient consumerClient, IEnumerable<XXXTopicPartition> partitions, CancellationToken stopCancellationToken);
 
         Task StopAsync();
