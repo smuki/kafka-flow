72293ee2d30bf33ce6d0f92b140d855cda3c7ab4
sunriseyuen <sunriseyuen@foxmail.com>
2020-12-31 19:43:04 +0800

    Create HandlerAttribute.cs

---
+++ src/KafkaFlow.TypedHandler/HandlerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaFlow.TypedHandler
+{
+    /// <summary>
+    /// Mark the class for dependency injection
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class HandlerAttribute : Attribute
+    {
+        /// <summary>
+        /// Sets if the service will be injected by interface or implementation. Default = Auto
+        /// </summary>
+        public int Priority { get; set; } = 100;
+    }
+}
