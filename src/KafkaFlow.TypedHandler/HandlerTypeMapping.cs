411caae1bf9b1b8d82c3f1a9a03ed0ca2ff44af9
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-05 19:56:03 +0800

    Update HandlerTypeMapping.cs

--- src/KafkaFlow.TypedHandler/HandlerTypeMapping.cs
+++ src/KafkaFlow.TypedHandler/HandlerTypeMapping.cs
@@ -3,35 +3,20 @@ namespace KafkaFlow.TypedHandler
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Reflection;
 
     public class HandlerTypeMapping
     {
-        private readonly Dictionary<Type, Type> mapping = new Dictionary<Type, Type>();
-        private readonly ConcurrentDictionary<Type, ICollection<IMessageHandler>> _eventHandlers = new ConcurrentDictionary<Type, ICollection<IMessageHandler>>();
-
+        private readonly ConcurrentDictionary<Type, ICollection<IMessageHandler>> _messageHandlers = new ConcurrentDictionary<Type, ICollection<IMessageHandler>>();
         private readonly IDependencyResolver dependencyResolver;
-
         public HandlerTypeMapping(
             IDependencyResolver dependencyResolver)
         {
             this.dependencyResolver = dependencyResolver;
         }
-
-        public void AddMapping(Type messageType, Type handlerType)
-        {
-            this.mapping.Add(messageType, handlerType);
-        }
-
-        public Type GetHandlerType(Type messageType)
-        {
-            return this.mapping.TryGetValue(messageType, out var handlerType) ? handlerType : null;
-        }
-
         public ICollection<IMessageHandler> GetHandlers(Type eventType)
         {
-            var eventHandlers = _eventHandlers.GetOrAdd(eventType, type =>
+            var eventHandlers = _messageHandlers.GetOrAdd(eventType, type =>
             {
                 ICollection<IMessageHandler> handlers = new List<IMessageHandler>();
                 foreach (var handlerType in dependencyResolver.Resolves<IMessageHandler>())
