cf13ac8f8745e1c526f3e2260491c8e755330f8d
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-05 19:51:22 +0800

    Update IKafkaConfigurationBuilder.cs

--- src/KafkaFlow.Abstractions/Configuration/IKafkaConfigurationBuilder.cs
+++ src/KafkaFlow.Abstractions/Configuration/IKafkaConfigurationBuilder.cs
@@ -12,12 +12,5 @@ namespace KafkaFlow.Configuration
         /// <param name="cluster"></param>
         /// <returns></returns>
         IKafkaConfigurationBuilder AddCluster(Action<IClusterConfigurationBuilder> cluster);
-
-        /// <summary>
-        /// Set the log handler to be used by the Framework, if none is provided the <see cref="NullLogHandler"/> will be used
-        /// </summary>
-        /// <typeparam name="TLogHandler">A class that implements the <see cref="ILogHandler"/> interface</typeparam>
-        /// <returns></returns>
-        //IKafkaConfigurationBuilder UseLogHandler<TLogHandler>() where TLogHandler : ILogHandler;
     }
 }
