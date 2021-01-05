7c27edd7b84f0cb8337dc93cc0f9a24c6438600b
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-05 20:09:25 +0800

    Update IProducerConfigurationBuilder.cs

--- src/KafkaFlow.Abstractions/Configuration/IProducerConfigurationBuilder.cs
+++ src/KafkaFlow.Abstractions/Configuration/IProducerConfigurationBuilder.cs
@@ -13,13 +13,6 @@ namespace KafkaFlow.Configuration
         IDependencyConfigurator DependencyConfigurator { get; }
 
         /// <summary>
-        /// Adds middlewares to the producer. The middlewares will be executed in the registration order
-        /// </summary>
-        /// <param name="middlewares">A handler to register middlewares</param>
-        /// <returns></returns>
-        //IProducerConfigurationBuilder AddMiddlewares(Action<IProducerMiddlewareConfigurationBuilder> middlewares);
-
-        /// <summary>
         /// Sets the default topic to be used when producing messages
         /// </summary>
         /// <param name="topic">Topic name</param>
