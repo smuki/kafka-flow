b9cf428a3d773f332b1c3ab72f7b110f6b41e1f9
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-05 15:19:09 +0800

    Update HandlerTypeMapping.cs

--- src/KafkaFlow.TypedHandler/HandlerTypeMapping.cs
+++ src/KafkaFlow.TypedHandler/HandlerTypeMapping.cs
@@ -1,11 +1,23 @@
 namespace KafkaFlow.TypedHandler
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
     public class HandlerTypeMapping
     {
         private readonly Dictionary<Type, Type> mapping = new Dictionary<Type, Type>();
+        private readonly ConcurrentDictionary<Type, ICollection<IMessageHandler>> _eventHandlers = new ConcurrentDictionary<Type, ICollection<IMessageHandler>>();
+
+        private readonly IDependencyResolver dependencyResolver;
+
+        public HandlerTypeMapping(
+            IDependencyResolver dependencyResolver)
+        {
+            this.dependencyResolver = dependencyResolver;
+        }
 
         public void AddMapping(Type messageType, Type handlerType)
         {
@@ -15,6 +27,26 @@ namespace KafkaFlow.TypedHandler
         public Type GetHandlerType(Type messageType)
         {
             return this.mapping.TryGetValue(messageType, out var handlerType) ? handlerType : null;
+        }
+
+        public ICollection<IMessageHandler> GetHandlers(Type eventType)
+        {
+            var eventHandlers = _eventHandlers.GetOrAdd(eventType, type =>
+            {
+                ICollection<IMessageHandler> handlers = new List<IMessageHandler>();
+                foreach (var handlerType in dependencyResolver.Resolves<IMessageHandler>())
+                {
+                    foreach (var implementedInterface in handlerType.GetType().GetTypeInfo().ImplementedInterfaces)
+                    {
+                        if (implementedInterface.IsGenericType && eventType.IsAssignableFrom(implementedInterface.GenericTypeArguments[0]))
+                        {
+                            handlers.Add(handlerType);
+                        }
+                    }
+                }
+                return handlers;
+            });
+            return eventHandlers;
         }
     }
 }
