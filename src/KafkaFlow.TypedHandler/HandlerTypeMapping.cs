0182cad0b1f3870e348b80f790aa275e7920236d
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-06 09:39:34 +0800

    Update HandlerTypeMapping.cs

--- src/KafkaFlow.TypedHandler/HandlerTypeMapping.cs
+++ src/KafkaFlow.TypedHandler/HandlerTypeMapping.cs
@@ -14,24 +14,30 @@ namespace KafkaFlow.TypedHandler
         {
             this.dependencyResolver = dependencyResolver;
         }
+        public ICollection<IMessageHandler> GetTypes(Type eventType)
+        {
+            ICollection<IMessageHandler> handlers = new List<IMessageHandler>();
+            var xx = dependencyResolver.Resolves<IMessageHandler>();
+            foreach (var handlerType in dependencyResolver.Resolves<IMessageHandler>())
+            {
+                Console.WriteLine("GetHandlers." + handlerType);
+
+                foreach (var implementedInterface in handlerType.GetType().GetTypeInfo().ImplementedInterfaces)
+                {
+                    if (implementedInterface.IsGenericType && eventType.IsAssignableFrom(implementedInterface.GenericTypeArguments[0]))
+                    {
+                        handlers.Add(handlerType);
+                    }
+                }
+            }
+
+            return handlers;
+        }
         public ICollection<IMessageHandler> GetHandlers(Type eventType)
         {
             var eventHandlers = _messageHandlers.GetOrAdd(eventType, type =>
             {
-                ICollection<IMessageHandler> handlers = new List<IMessageHandler>();
-                foreach (var handlerType in dependencyResolver.Resolves<IMessageHandler>())
-                {
-                    Console.WriteLine("GetHandlers." + handlerType);
-
-                    foreach (var implementedInterface in handlerType.GetType().GetTypeInfo().ImplementedInterfaces)
-                    {
-                        if (implementedInterface.IsGenericType && eventType.IsAssignableFrom(implementedInterface.GenericTypeArguments[0]))
-                        {
-                            handlers.Add(handlerType);
-                        }
-                    }
-                }
-                return handlers;
+                return this.GetTypes(eventType);
             });
             return eventHandlers;
         }
