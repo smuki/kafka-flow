8e581990cb7fbfdaa91ef74ccb158ecd4c272442
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 21:44:36 +0800

    Update TraceLogHandler.cs

--- src/KafkaFlow.IntegrationTests/Core/TraceLogHandler.cs
+++ src/KafkaFlow.IntegrationTests/Core/TraceLogHandler.cs
@@ -1,43 +1,43 @@
-namespace KafkaFlow.IntegrationTests.Core
-{
-    using System;
-    using System.Diagnostics;
-    using System.Text.Json;
+//namespace KafkaFlow.IntegrationTests.Core
+//{
+//    using System;
+//    using System.Diagnostics;
+//    using System.Text.Json;
 
-    public class TraceLogHandler : ILogHandler
-    {
-        public void Error(string message, Exception ex, object data)
-        {
-            Trace.TraceError(
-                JsonSerializer.Serialize(
-                    new
-                    {
-                        Message = message,
-                        Exception = ex,
-                        Data = data
-                    }));
-        }
+//    public class TraceLogHandler : ILogHandler
+//    {
+//        public void Error(string message, Exception ex, object data)
+//        {
+//            Trace.TraceError(
+//                JsonSerializer.Serialize(
+//                    new
+//                    {
+//                        Message = message,
+//                        Exception = ex,
+//                        Data = data
+//                    }));
+//        }
 
-        public void Info(string message, object data)
-        {
-            Trace.TraceInformation(
-                JsonSerializer.Serialize(
-                    new
-                    {
-                        Message = message,
-                        Data = data
-                    }));
-        }
+//        public void Info(string message, object data)
+//        {
+//            Trace.TraceInformation(
+//                JsonSerializer.Serialize(
+//                    new
+//                    {
+//                        Message = message,
+//                        Data = data
+//                    }));
+//        }
 
-        public void Warning(string message, object data)
-        {
-            Trace.TraceWarning(
-                JsonSerializer.Serialize(
-                    new
-                    {
-                        Message = message,
-                        Data = data
-                    }));
-        }
-    }
-}
+//        public void Warning(string message, object data)
+//        {
+//            Trace.TraceWarning(
+//                JsonSerializer.Serialize(
+//                    new
+//                    {
+//                        Message = message,
+//                        Data = data
+//                    }));
+//        }
+//    }
+//}
