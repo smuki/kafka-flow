7c0bfc4a2840874c75857ed0cc6235f6bfd0eaea
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 11:12:02 +0800

    Update ServiceCollectionExtensions.cs

--- src/KafkaFlow.Abstractions/Extensions/ServiceCollectionExtensions.cs
+++ src/KafkaFlow.Abstractions/Extensions/ServiceCollectionExtensions.cs
@@ -11,38 +11,6 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        /// <summary>
-        /// Registers the specified service only if none already exists for the specified provider type.
-        /// </summary>
-        public static IServiceCollection TryAddProvider<TService, TProvider>(
-            this IServiceCollection services,
-            ServiceLifetime lifetime)
-        {
-            return services.TryAddProvider(typeof(TService), typeof(TProvider), lifetime);
-        }
-
-        /// <summary>
-        /// Registers the specified service only if none already exists for the specified provider type.
-        /// </summary>
-        public static IServiceCollection TryAddProvider(
-            this IServiceCollection services,
-            Type serviceType,
-            Type providerType,
-            ServiceLifetime lifetime)
-        {
-            var descriptor = services.FirstOrDefault(
-                x => x.ServiceType == serviceType && x.ImplementationType == providerType
-            );
-
-            if (descriptor == null)
-            {
-                descriptor = new ServiceDescriptor(serviceType, providerType, lifetime);
-                services.Add(descriptor);
-            }
-
-            return services;
-        }
-
         public static IServiceCollection UseKafkaFlow(this IServiceCollection services)
         {
             var consumerManager = new ConsumerManager();
@@ -52,13 +20,11 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddSingleton<IConsumerAccessor>(consumerManager);
             services.AddSingleton<IConsumerAccessor>(consumerManager);
             services.AddSingleton<IConsumerManager>(consumerManager);
-            services.AddSingleton<IProducerAccessor, ProducerAccessor>();
                     
             return services;
         }
 
         public static IServiceCollection Replace<TService, TImplementation>(this IServiceCollection services, ServiceLifetime lifetime) => services.Replace(new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime));
         public static bool HasService<T>(this IServiceCollection services) => services.Any(x => x.ServiceType == typeof(T));
-        //public static bool HasService<T>(this KafkaFlowOptions options) => options.Services.HasService<T>();
     }
 }
