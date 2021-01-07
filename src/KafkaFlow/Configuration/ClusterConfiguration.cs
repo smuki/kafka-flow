6c0d189dbc5d1053d02e425a2ccddc7d7f4b6dda
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 10:08:04 +0800

    Update ClusterConfiguration.cs

--- src/KafkaFlow/Configuration/ClusterConfiguration.cs
+++ src/KafkaFlow/Configuration/ClusterConfiguration.cs
@@ -17,20 +17,12 @@ namespace KafkaFlow.Configuration
         {
             this.securityInformationHandler = securityInformationHandler;
             this.Kafka = kafka;
-            this.Brokers = brokers.ToList();
         }
 
         public KafkaConfiguration Kafka { get; }
-
-        public IReadOnlyCollection<string> Brokers { get; }
-
         public IReadOnlyCollection<ProducerConfiguration> Producers => this.producers.AsReadOnly();
 
         public IReadOnlyCollection<ConsumerConfiguration> Consumers => this.consumers.AsReadOnly();
-
-        //public void AddConsumers(IEnumerable<ConsumerConfiguration> configurations) => this.consumers.AddRange(configurations);
-
-        //public void AddProducers(IEnumerable<ProducerConfiguration> configurations) => this.producers.AddRange(configurations);
 
         public SecurityInformation GetSecurityInformation() => this.securityInformationHandler?.Invoke();
     }
