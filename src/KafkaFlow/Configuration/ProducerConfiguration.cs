5351f1d54ae1628e987202da37416bf8ded1d5f0
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 10:08:22 +0800

    Update ProducerConfiguration.cs

--- src/KafkaFlow/Configuration/ProducerConfiguration.cs
+++ src/KafkaFlow/Configuration/ProducerConfiguration.cs
@@ -14,40 +14,11 @@ namespace KafkaFlow.Configuration
             IReadOnlyList<Action<string>> statisticsHandlers)
         {
             this.Cluster = cluster ?? throw new ArgumentNullException(nameof(cluster));
-            //this.DefaultTopic = defaultTopic;
-            //this.Acks = acks;
-            //this.BaseProducerConfig = baseProducerConfig;
-            //this.StatisticsHandlers = statisticsHandlers;
         }
 
         public ClusterConfiguration Cluster { get; }
 
         public ProducerConfig BaseProducerConfig { get; }
-
-        public ProducerConfig GetKafkaConfig()
-        {
-            this.BaseProducerConfig.BootstrapServers = string.Join(",", this.Cluster.Brokers);
-            //this.BaseProducerConfig.Acks = ParseAcks(this.Acks);
-
-            return this.BaseProducerConfig;
-        }
-
-        //private static Confluent.Kafka.Acks? ParseAcks(Acks? acks)
-        //{
-        //    switch (acks)
-        //    {
-        //        case KafkaFlow.Acks.Leader:
-        //            return Confluent.Kafka.Acks.Leader;
-
-        //        case KafkaFlow.Acks.All:
-        //            return Confluent.Kafka.Acks.All;
-
-        //        case KafkaFlow.Acks.None:
-        //            return Confluent.Kafka.Acks.None;
-
-        //        default:
-        //            return null;
-        //    }
-        //}
+      
     }
 }
