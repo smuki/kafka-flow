c50180dc93dbd7e1604d891189bf1a9580109e7e
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 10:03:34 +0800

    Update IProducerConfigurationBuilder.cs

--- src/KafkaFlow.Abstractions/Configuration/IProducerConfigurationBuilder.cs
+++ src/KafkaFlow.Abstractions/Configuration/IProducerConfigurationBuilder.cs
@@ -12,32 +12,5 @@ namespace KafkaFlow.Configuration
         /// </summary>
         IDependencyConfigurator DependencyConfigurator { get; }
 
-        /// <summary>
-        /// Sets the default topic to be used when producing messages
-        /// </summary>
-        /// <param name="topic">Topic name</param>
-        /// <returns></returns>
-        IProducerConfigurationBuilder DefaultTopic(string topic);
-
-        /// <summary>
-        /// Sets the <see cref="Acks"/> to be used when producing messages
-        /// </summary>
-        /// <param name="acks"></param>
-        /// <returns></returns>
-        //IProducerConfigurationBuilder WithAcks(Acks acks);
-
-        /// <summary>
-        /// Adds a handler for the Kafka producer statistics
-        /// </summary>
-        /// <param name="statisticsHandler">A handler for the statistics</param>
-        /// <returns></returns>
-        IProducerConfigurationBuilder WithStatisticsHandler(Action<string> statisticsHandler);
-
-        /// <summary>
-        /// Sets the interval the statistics are emitted
-        /// </summary>
-        /// <param name="statisticsIntervalMs">The interval in miliseconds</param>
-        /// <returns></returns>
-        IProducerConfigurationBuilder WithStatisticsIntervalMs(int statisticsIntervalMs);
     }
 }
