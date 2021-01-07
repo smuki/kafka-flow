5fb843a3334573c9fa4cc24d1e9e90d827de8214
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 09:25:54 +0800

    Update MessageQueueSettting.cs

--- src/KafkaFlow.Abstractions/Configuration/MessageQueueSettting.cs
+++ src/KafkaFlow.Abstractions/Configuration/MessageQueueSettting.cs
@@ -10,14 +10,8 @@ namespace KafkaFlow.Configuration
     {
         public string Topic { get; set; }
         public string Servers { get; set; }
-        public string GroupId { get; set; }
-        public string ConsumerName { get; set; }
-        public int WorkerCount { get; set; }
-        public int BufferSize { get; set; }
-        public bool AutoStoreOffsets { get; set; }
-        public TimeSpan AutoCommitInterval { get; set; }
+
         private readonly Dictionary<string, string> _Parameter = new Dictionary<string, string>();
-        public Factory<IDistributionStrategy> DistributionStrategyFactory { get; set; }
         MessageConsumerSettting MessageConsumer { get; set; }
         MessageProducerSettting MessageProducer { get; set; }
         public string this[string name]
@@ -29,9 +23,15 @@ namespace KafkaFlow.Configuration
         }
         public MessageQueueSettting(IConfigurationSection conf)
         {
-            this.Topic = conf.Get("topic");
-            this.Servers = conf.Get("servers");
-            this.WorkerCount = Util.ToInt(conf.Get("WorkerCount"));
+            foreach (var it in conf.GetChildren())
+            {
+                if (!string.IsNullOrWhiteSpace(it.Value))
+                {
+                    _Parameter[it.Key] = it.Value;
+                }
+            }
+            this.Topic = this["topic"];
+            this.Servers = this["servers"];
 
             this.MessageProducer = new MessageProducerSettting(conf.GetSection("producer"));
             this.MessageProducer.Topic = this.Topic;
