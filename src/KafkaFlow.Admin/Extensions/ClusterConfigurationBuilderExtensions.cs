09e8949b3079fb2ccaf3d1334e624be5d518cac0
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 11:03:07 +0800

    update

--- src/KafkaFlow.Admin/Extensions/ClusterConfigurationBuilderExtensions.cs
+++ src/KafkaFlow.Admin/Extensions/ClusterConfigurationBuilderExtensions.cs
@@ -1,32 +1,32 @@
-﻿namespace KafkaFlow
-{
-    using System;
-    using System.Reflection;
-    using KafkaFlow.Admin;
-    using KafkaFlow.Admin.Handlers;
-    using KafkaFlow.Configuration;
-    using KafkaFlow.Serializer;
-    using KafkaFlow.Serializer.ProtoBuf;
-    using KafkaFlow.TypedHandler;
-    using KafkaFlow.Dependency;
+﻿//namespace KafkaFlow
+//{
+//    using System;
+//    using System.Reflection;
+//    using KafkaFlow.Admin;
+//    using KafkaFlow.Admin.Handlers;
+//    using KafkaFlow.Configuration;
+//    using KafkaFlow.Serializer;
+//    using KafkaFlow.Serializer.ProtoBuf;
+//    using KafkaFlow.TypedHandler;
+//    using KafkaFlow.Dependency;
 
-    public static class ClusterConfigurationBuilderExtensions
-    {
-        public static IClusterConfigurationBuilder EnableAdminMessages(
-            this IClusterConfigurationBuilder cluster,
-            string adminTopic,
-            string adminConsumerGroup)
-        {
-            cluster.DependencyConfigurator.AddSingleton<IAdminProducer, AdminProducer>();
+//    public static class ClusterConfigurationBuilderExtensions
+//    {
+//        public static IClusterConfigurationBuilder EnableAdminMessages(
+//            this IClusterConfigurationBuilder cluster,
+//            string adminTopic,
+//            string adminConsumerGroup)
+//        {
+//            cluster.DependencyConfigurator.AddSingleton<IAdminProducer, AdminProducer>();
 
-            return cluster;
-        }
+//            return cluster;
+//        }
 
-        public static IClusterConfigurationBuilder EnableAdminMessages(
-            this IClusterConfigurationBuilder cluster,
-            string adminTopic)
-        {
-            return cluster.EnableAdminMessages(adminTopic, $"Admin-{Assembly.GetEntryAssembly().GetName().Name}");
-        }
-    }
-}
+//        public static IClusterConfigurationBuilder EnableAdminMessages(
+//            this IClusterConfigurationBuilder cluster,
+//            string adminTopic)
+//        {
+//            return cluster.EnableAdminMessages(adminTopic, $"Admin-{Assembly.GetEntryAssembly().GetName().Name}");
+//        }
+//    }
+//}
