b8919a6480be6e37d7c23449c9dc78ca1f619fe2
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 11:02:57 +0800

    Update ServiceCollectionExtensions.cs

--- samples/KafkaFlow.Sample/ServiceCollectionExtensions.cs
+++ samples/KafkaFlow.Sample/ServiceCollectionExtensions.cs
@@ -1,25 +1,25 @@
-namespace KafkaFlow
-{
-    using System;
-    using global::Microsoft.Extensions.DependencyInjection;
-    using KafkaFlow.Configuration;
-    using KafkaFlow.Microsoft.DependencyInjection;
+//namespace KafkaFlow
+//{
+//    using System;
+//    using global::Microsoft.Extensions.DependencyInjection;
+//    using KafkaFlow.Configuration;
+//    using KafkaFlow.Microsoft.DependencyInjection;
 
-    /// <summary>
-    /// Extension methods over IServiceCollection
-    /// </summary>
-    public static class ServiceCollectionExtensions
-    {
-        /// <summary>
-        /// Configures KafkaFlow
-        /// </summary>
-        /// <param name="services">Instance of <see cref="IServiceCollection"/></param>
-        /// <param name="kafka">A handler to configure KafkaFlow</param>
-        /// <returns></returns>
-        public static IServiceCollection AddKafka(this IServiceCollection services, Action<IKafkaConfigurationBuilder> kafka)
-        {
-            var configurator = new KafkaFlowConfigurator(new MicrosoftDependencyConfigurator(services), kafka);
-            return services.AddSingleton(configurator);
-        }
-    }
-}
+//    /// <summary>
+//    /// Extension methods over IServiceCollection
+//    /// </summary>
+//    public static class ServiceCollectionExtensions
+//    {
+//        /// <summary>
+//        /// Configures KafkaFlow
+//        /// </summary>
+//        /// <param name="services">Instance of <see cref="IServiceCollection"/></param>
+//        /// <param name="kafka">A handler to configure KafkaFlow</param>
+//        /// <returns></returns>
+//        public static IServiceCollection AddKafka(this IServiceCollection services, Action<IKafkaConfigurationBuilder> kafka)
+//        {
+//            var configurator = new KafkaFlowConfigurator(new MicrosoftDependencyConfigurator(services), kafka);
+//            return services.AddSingleton(configurator);
+//        }
+//    }
+//}
