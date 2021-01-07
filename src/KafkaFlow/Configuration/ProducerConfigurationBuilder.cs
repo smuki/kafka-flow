83d6e05db4fb7b68b43604a2247317276791c9f5
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 10:15:16 +0800

    Update ProducerConfigurationBuilder.cs

--- src/KafkaFlow/Configuration/ProducerConfigurationBuilder.cs
+++ src/KafkaFlow/Configuration/ProducerConfigurationBuilder.cs
@@ -1,55 +1,55 @@
-namespace KafkaFlow.Configuration
-{
-    using System;
-    using System.Collections.Generic;
-    using Confluent.Kafka;
+//namespace KafkaFlow.Configuration
+//{
+//    using System;
+//    using System.Collections.Generic;
+//    using Confluent.Kafka;
 
-    public class ProducerConfigurationBuilder : IProducerConfigurationBuilder
-    {
-        private readonly string name;
-        private readonly List<Action<string>> statisticsHandlers = new List<Action<string>>();
+//    public class ProducerConfigurationBuilder : IProducerConfigurationBuilder
+//    {
+//        private readonly string name;
+//        private readonly List<Action<string>> statisticsHandlers = new List<Action<string>>();
 
-        private string topic;
-        private ProducerConfig producerConfig;
-        private Acks? acks;
-        private int statisticsInterval;
+//        private string topic;
+//        private ProducerConfig producerConfig;
+//        private Acks? acks;
+//        private int statisticsInterval;
 
-        public ProducerConfigurationBuilder(IDependencyConfigurator dependencyConfigurator, string name)
-        {
-            this.name = name;
-            this.DependencyConfigurator = dependencyConfigurator;
-        }
+//        public ProducerConfigurationBuilder(IDependencyConfigurator dependencyConfigurator, string name)
+//        {
+//            this.name = name;
+//            this.DependencyConfigurator = dependencyConfigurator;
+//        }
 
-        public IDependencyConfigurator DependencyConfigurator { get; }
+//        public IDependencyConfigurator DependencyConfigurator { get; }
 
-        public IProducerConfigurationBuilder DefaultTopic(string topic)
-        {
-            this.topic = topic;
-            return this;
-        }
+//        public IProducerConfigurationBuilder DefaultTopic(string topic)
+//        {
+//            this.topic = topic;
+//            return this;
+//        }
 
-        public IProducerConfigurationBuilder WithProducerConfig(ProducerConfig config)
-        {
-            this.producerConfig = config;
-            return this;
-        }
+//        public IProducerConfigurationBuilder WithProducerConfig(ProducerConfig config)
+//        {
+//            this.producerConfig = config;
+//            return this;
+//        }
 
-        public IProducerConfigurationBuilder WithAcks(Acks acks)
-        {
-            this.acks = acks;
-            return this;
-        }
+//        public IProducerConfigurationBuilder WithAcks(Acks acks)
+//        {
+//            this.acks = acks;
+//            return this;
+//        }
 
-        public IProducerConfigurationBuilder WithStatisticsHandler(Action<string> statisticsHandler)
-        {
-            this.statisticsHandlers.Add(statisticsHandler);
-            return this;
-        }
+//        public IProducerConfigurationBuilder WithStatisticsHandler(Action<string> statisticsHandler)
+//        {
+//            this.statisticsHandlers.Add(statisticsHandler);
+//            return this;
+//        }
 
-        public IProducerConfigurationBuilder WithStatisticsIntervalMs(int statisticsIntervalMs)
-        {
-            this.statisticsInterval = statisticsIntervalMs;
-            return this;
-        }
-    }
-}
+//        public IProducerConfigurationBuilder WithStatisticsIntervalMs(int statisticsIntervalMs)
+//        {
+//            this.statisticsInterval = statisticsIntervalMs;
+//            return this;
+//        }
+//    }
+//}
