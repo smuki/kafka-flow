c2485a7ee348d3dd127beafe8d6df1a1bed3865a
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 10:15:08 +0800

    Update ProducerConfiguration.cs

--- src/KafkaFlow/Configuration/ProducerConfiguration.cs
+++ src/KafkaFlow/Configuration/ProducerConfiguration.cs
@@ -1,24 +1,23 @@
-namespace KafkaFlow.Configuration
-{
-    using System;
-    using System.Collections.Generic;
-    using Confluent.Kafka;
-    //using Acks = KafkaFlow.Acks;
+//namespace KafkaFlow.Configuration
+//{
+//    using System;
+//    using System.Collections.Generic;
+//    using Confluent.Kafka;
 
-    public class ProducerConfiguration: MessageProducerSettting
-    {
-        public ProducerConfiguration(
-            ClusterConfiguration cluster,
-            Acks? acks,
-            ProducerConfig baseProducerConfig,
-            IReadOnlyList<Action<string>> statisticsHandlers)
-        {
-            this.Cluster = cluster ?? throw new ArgumentNullException(nameof(cluster));
-        }
+//    public class ProducerConfiguration: MessageProducerSettting
+//    {
+//        public ProducerConfiguration(
+//            ClusterConfiguration cluster,
+//            Acks? acks,
+//            ProducerConfig baseProducerConfig,
+//            IReadOnlyList<Action<string>> statisticsHandlers)
+//        {
+//            this.Cluster = cluster ?? throw new ArgumentNullException(nameof(cluster));
+//        }
 
-        public ClusterConfiguration Cluster { get; }
+//        public ClusterConfiguration Cluster { get; }
 
-        public ProducerConfig BaseProducerConfig { get; }
+//        public ProducerConfig BaseProducerConfig { get; }
       
-    }
-}
+//    }
+//}
