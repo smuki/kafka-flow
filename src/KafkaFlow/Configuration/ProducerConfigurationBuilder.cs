2ba30b0b230fdb0cd39b3698b4f5c2f4ab6df5ed
sunriseyuen <sunriseyuen@foxmail.com>
2021-01-07 09:10:31 +0800

    Update ProducerConfigurationBuilder.cs

--- src/KafkaFlow/Configuration/ProducerConfigurationBuilder.cs
+++ src/KafkaFlow/Configuration/ProducerConfigurationBuilder.cs
@@ -3,12 +3,10 @@ namespace KafkaFlow.Configuration
     using System;
     using System.Collections.Generic;
     using Confluent.Kafka;
-    using Acks = KafkaFlow.Acks;
 
     public class ProducerConfigurationBuilder : IProducerConfigurationBuilder
     {
         private readonly string name;
-        //private readonly ProducerMiddlewareConfigurationBuilder middlewareConfigurationBuilder;
         private readonly List<Action<string>> statisticsHandlers = new List<Action<string>>();
 
         private string topic;
@@ -20,7 +18,6 @@ namespace KafkaFlow.Configuration
         {
             this.name = name;
             this.DependencyConfigurator = dependencyConfigurator;
-            //this.middlewareConfigurationBuilder = new ProducerMiddlewareConfigurationBuilder(dependencyConfigurator);
         }
 
         public IDependencyConfigurator DependencyConfigurator { get; }
@@ -54,23 +51,5 @@ namespace KafkaFlow.Configuration
             this.statisticsInterval = statisticsIntervalMs;
             return this;
         }
-
-        //public ProducerConfiguration Build(ClusterConfiguration clusterConfiguration)
-        //{
-        //    this.producerConfig ??= new ProducerConfig();
-        //    this.producerConfig.StatisticsIntervalMs = this.statisticsInterval;
-
-        //    this.producerConfig.ReadSecurityInformation(clusterConfiguration);
-
-        //    var configuration = new ProducerConfiguration(
-        //        clusterConfiguration,
-        //        this.name,
-        //        this.topic,
-        //        this.acks,
-        //        this.producerConfig,
-        //        this.statisticsHandlers);
-
-        //    return configuration;
-        //}
     }
 }
